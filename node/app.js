c85f1d738776c7bdd4f42551c3b3ca746be81ab9
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2025-11-27 11:34:20 -0500

    ejemplo docker

---
+++ node/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hola desde Node.js dentro de un contenedor Docker");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Servidor eejcutado en puero " + PORT);
+})
